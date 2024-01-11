744e2c81fb4dc562068cf27ccc754dbd71ec33cb
Chojnacki <twojemail@example.com>
2024-01-11 16:56:19 +0100

    ...

---
+++ src/main/java/main.java
@@ -0,0 +1,25 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.print("|  ");
+        System.out.print("|  ");
+        System.out.print("|  ");
+        System.out.print("|  ");
+
+        System.out.println("");
+        System.out.print("|  ");
+        System.out.print("|  ");
+        System.out.print("|  ");
+        System.out.print("|  ");
+
+        System.out.println("");
+        System.out.print("|  ");
+        System.out.print("|  ");
+        System.out.print("|  ");
+        System.out.print("|  ");
+
+
+
+    }
+}
